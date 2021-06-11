43c1b8a192c3f51e50db3cd28b143b72dae2122c
amirbangash <amirbangash019@gmail.com>
2021-06-11 18:52:51 -0400

    Header Section - Navbar & Create Links to Section

--- js/script.js
+++ js/script.js
@@ -1,31 +1,69 @@
-/*------------------- about section tabs -------------------*/
+/*------------------- Toggler Navbar -------------------*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+  document.querySelector(".header").classList.toggle("active");
+}
 
-(() => {
-  const aboutSection = document.querySelector(".about-section"),
-    tabsContainer = document.querySelector(".about-tabs");
-
-  tabsContainer.addEventListener("click", (event) => {
-    /* if event.target contains tab items class and not contains active class */
-    if (
-      event.target.classList.contains("tab-item") &&
-      !event.target.classList.contains("active")
-    ) {
-      const target = event.target.getAttribute("data-target");
-      // deactivate existing active 'tab-item'
-      tabsContainer
-        .querySelector(".active")
-        .classList.remove("outer-shadow", "active");
-      // activate new 'tab item'
-      event.target.classList.add("active", "outer-shadow");
-      // deactivate existing active 'tab-content'
-      aboutSection
-        .querySelector(".tab-content.active")
-        .classList.remove("active");
-      //  activate new tab-content
-      aboutSection.querySelector(target).classList.add("active");
+/*------------------- Active Section -------------------*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
     }
-  });
-})();
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+})(
+  /*------------------- about section tabs -------------------*/
+
+  () => {
+    const aboutSection = document.querySelector(".about-section"),
+      tabsContainer = document.querySelector(".about-tabs");
+
+    tabsContainer.addEventListener("click", (event) => {
+      /* if event.target contains tab items class and not contains active class */
+      if (
+        event.target.classList.contains("tab-item") &&
+        !event.target.classList.contains("active")
+      ) {
+        const target = event.target.getAttribute("data-target");
+        // deactivate existing active 'tab-item'
+        tabsContainer
+          .querySelector(".active")
+          .classList.remove("outer-shadow", "active");
+        // activate new 'tab item'
+        event.target.classList.add("active", "outer-shadow");
+        // deactivate existing active 'tab-content'
+        aboutSection
+          .querySelector(".tab-content.active")
+          .classList.remove("active");
+        //  activate new tab-content
+        aboutSection.querySelector(target).classList.add("active");
+      }
+    });
+  }
+)();
 
 /*------------------- portfolio item details popup -------------------*/
 document.addEventListener("click", (e) => {
